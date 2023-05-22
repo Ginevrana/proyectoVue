b7e49dc9ab8889763fc7f1813680642473b7b7c1
Florencia Teresita Sanchez <florenciateresita@gmail.com>
2023-05-22 11:05:27 -0300

    :rocket: Comienzo de proyecto Vue para práctica de framework

---
+++ introduccion.js
@@ -0,0 +1,5 @@
+/*
+Introducción a Vue.js
+Framework para crear interfaces de usuario, diseñado para trabajar de manera incremental y progresiva.
+Biblioteca central enfocada sólo en la capa de la vista y fácil de integrar con otros proyectos existentes.
+*/
